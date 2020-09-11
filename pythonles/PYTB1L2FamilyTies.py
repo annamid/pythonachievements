ad252682623c7966f8cb41f2e568dcc8cc1ee611
anna <annamiddelkoop@gmail.com>
2020-09-11 12:45:47 +0200

    mappen gemaakt en opdracht toegevoegd

---
+++ pythonles/PYTB1L2FamilyTies.py
@@ -0,0 +1,18 @@
+# je naam
+# je leeftijd 
+# je woonplaats en afstand van school in km met 1 komma
+
+
+#string, float, int
+
+naam = "Anna Middelkoop"
+kilometers = 27.1
+leeftijd = 19
+thuis = "Nieuw-Vennep"
+tijd = 25
+
+print("Hallo, mijn naam is" + " " + naam)
+print("Ik ben " + str(leeftijd) + " " + "jaar oud, en woon in " + thuis) 
+print("Vanaf " + thuis + " " + "naar het Mediacollege is het " + str(kilometers) + "km")
+print("ALs ik met de trein naar school moet ben ik " + str(tijd) + " minuten onderweg, samen met Oscar")
+
