965eb04ede6e05d9c34f0f23641bbbc5f6a72ef4
anna <annamiddelkoop@gmail.com>
2020-10-02 12:11:42 +0200

    changes

---
+++ pythonDailychoices.py
@@ -0,0 +1,46 @@
+print("good morning! It's a school day and your dog wakes you up")
+print("do you WALK your dog first or do you SHOWER first?")
+choice = input()
+if choice == 'WALK':
+    print("you walk your dog and feel the nice cold air in the morning")
+elif choice == 'SHOWER': 
+    print("you take a warm and feel fresh")
+else:
+    print("not a valid choice")
+
+print("you need to eat something. Do you want bread with CHOCOLATE or CHEESE?")
+choice = input()
+if choice == 'CHOCOLATE':
+     print("you enjoy the sweet taste")
+elif choice == 'CHEESE':
+     print("you enjoy the taste of the cheese")
+else:   
+    print("not a valid choice")  
+
+print("almost late for school! do you take the TRAIN or the BUS?")
+choice = input()
+if choice == 'TRAIN':
+     print("you are on time")
+elif choice == 'BUS':
+     print("you are 5 minutes late")
+else:   
+    print("not a valid choice")
+
+print("where do you want to sit? In the BACK or in the FRONT of the class")
+choice = input()
+if choice == 'BACK':
+     print("you sit at the back of the class")
+elif choice == 'FRONT':
+     print("you take a seat in the front")
+else:   
+    print("not a valid choice")    
+
+print("school is over! do you want to go straight HOME or STAY to hang out with some friends?")
+choice = input()
+if choice == 'HOME':
+     print("you take the train home and relax")
+elif choice == 'STAY':
+     print("you talk some more about yourself with your friends and go home after")
+else:   
+    print("Not a valid choice")  
+
